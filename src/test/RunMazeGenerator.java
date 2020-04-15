a597148390443e4a3ad9d921445ed8ac9770b318
Mor Saranga <morlisa@post.bgu.ac.il>
2020-04-15 20:35:07 +0300

    Changes on 15.4: Fixing the maze generating on MyMazeGenerator. Also changed behavior for DFS. Ran a few tests

--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -9,10 +9,6 @@ public class RunMazeGenerator {
         testMazeGenerator(new MyMazeGenerator());
     }
     private static void testMazeGenerator(IMazeGenerator mazeGenerator) {
-        // TEST 15.4
-
-
-
         // prints the time it takes the algorithm to run
         System.out.println(String.format("Maze generation time(ms): %s",
                 mazeGenerator.measureAlgorithmTimeMillis(30/*rows*/,30/*columns*/)));

be930c97b2ea2e1bc644e23fdb46075d624e70dc
Mor Saranga <morlisa@post.bgu.ac.il>
2020-05-26 20:00:00 +0300

    Changes on 26.5.20: Creating the new Server package and its classes

---
+++ src/Server/Server.java
@@ -0,0 +1,13 @@
+package Server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    Socket clientSocket;
+    ServerSocket MyServer;
+
+//    try{
+//       // MyServer = new ServerSocket(PortNumber);
+//    }
+}
